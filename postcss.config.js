6a30a8dc8c43bc2930c0dda44cc5f99f6e986190
Mohammad-Mazraani <mohammad.mazraani@st.ul.edu.lb>
2025-09-14 03:20:11 +0300

    Fix configuration files and add proper .gitignore

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default {
   plugins: {
     tailwindcss: {},
@@ -6,12 +6,4 @@ export default {
   }
 }
 
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  }
-}
 
->>>>>>> 9b1e05fcc4901b7ebe6cc23d2be9e84a2c2555c2
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
+
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-=======
-export default {
-  content: [
-    "./index.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 9b1e05fcc4901b7ebe6cc23d2be9e84a2c2555c2
 }
